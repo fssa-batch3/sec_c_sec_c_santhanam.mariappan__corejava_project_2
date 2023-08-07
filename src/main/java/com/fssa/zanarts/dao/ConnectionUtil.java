08ad83694ebe51b11e2327921cf5da008f652bd7
Santhanam <116251909+santhanam13@users.noreply.github.com>
2023-08-07 19:10:31 +0530

    Add files via upload

---
+++ src/main/java/com/fssa/zanarts/dao/ConnectionUtil.java
@@ -0,0 +1,62 @@
+package com.fssa.zanarts.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+/**
+ * Establishes a database connection.
+ *
+ * @return A Connection object representing the database connection.
+ * @throws RuntimeException If unable to connect to the database.
+ */
+public class ConnectionUtil {
+//	public static Connection getConnection() {
+//		Connection con = null;
+//		String url = "jdbc:mysql://localhost:3306/Zanarts";
+//		String userName = "root";
+//		String passWord = "123456";
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			con = DriverManager.getConnection(url, userName, passWord);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			throw new RuntimeException("Unable to connect to the database");
+//		}
+//		System.out.println("connection successful");
+//		return con;
+//	}
+
+	public static Connection getConnection() {
+		Connection con = null;
+
+		String url, userName, passWord;
+
+		if (System.getenv("CI") != null) {
+			url = System.getenv("DATABASE_HOST");
+			userName = System.getenv("DATABASE_USERNAME");
+			passWord = System.getenv("DATABASE_PASSWORD");
+		} else {
+			Dotenv env = Dotenv.load();
+			url = env.get("DATABASE_HOST");
+			userName = env.get("DATABASE_USERNAME");
+			passWord = env.get("DATABASE_PASSWORD");
+		}
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(url, userName, passWord);
+			System.out.println("connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to connect to the database");
+		}
+		return con;
+	}
+
+	public static void main(String[] args) {
+		getConnection();
+	}
+
+}
---
+++ src/main/java/com/fssa/zanarts/service/ProductService.java
@@ -0,0 +1,96 @@
+package com.fssa.zanarts.service;
+
+import java.sql.SQLException;
+
+import java.time.LocalDateTime;
+
+import com.fssa.zanarts.customexception.CustomExpection;
+import com.fssa.zanarts.dao.ProductDao;
+import com.fssa.zanarts.model.Dimension;
+import com.fssa.zanarts.model.Product;
+import com.fssa.zanarts.validator.ProductValidator;
+
+/**
+ * This class provides methods to manage product-related operations.
+ */
+
+public class ProductService {
+
+	/**
+	 * Adds a new product to the database.
+	 *
+	 * @param product The product to be added.
+	 * @return {@code true} if the product was successfully added, {@code false}
+	 *         otherwise.
+	 * @throws SQLException    If a database error occurs.
+	 * @throws CustomExpection If there is a custom validation error.
+	 */
+
+	public static boolean addproduct(Product product) throws SQLException, CustomExpection {
+		if (ProductValidator.validate(product)) {
+			ProductDao.addProduct(product);
+		}
+		return true;
+	}
+
+	/**
+	 * Updates an existing product in the database.
+	 *
+	 * @param product The product to be updated.
+	 * @return {@code true} if the product was successfully updated, {@code false}
+	 *         otherwise.
+	 * @throws SQLException    If a database error occurs.
+	 * @throws CustomExpection If there is a custom validation error.
+	 */
+	public static boolean updateProduct(Product product) throws SQLException, CustomExpection {
+		if (ProductValidator.validate(product)) {
+			ProductDao.updateProduct(product);
+		}
+		return true;
+	}
+
+	/**
+	 * Retrieves details of all products from the database.
+	 *
+	 * @return {@code true} if product details were successfully retrieved,
+	 *         {@code false} otherwise.
+	 * @throws SQLException    If a database error occurs.
+	 * @throws CustomExpection If there is a custom validation error.
+	 */
+	public static boolean getAllProductDetails() throws SQLException, CustomExpection {
+		ProductDao.getAllProductDetails();
+		return true;
+	}
+
+	/**
+	 * Deletes a product from the database by its ID.
+	 *
+	 * @param productId The ID of the product to be deleted.
+	 * @return {@code true} if the product was successfully deleted, {@code false}
+	 *         otherwise.
+	 * @throws SQLException           If a database error occurs.
+	 * @throws IllegalAccessException If the operation is not allowed due to access
+	 *                                restrictions.
+	 * @throws CustomExpection        If there is a custom validation error.
+	 */
+	public static boolean deleteProduct(int productId) throws SQLException, IllegalAccessException, CustomExpection {
+		if (ProductValidator.validateProductId(productId)) {
+			ProductDao.deleteProduct(productId);
+		}
+		return true;
+	}
+
+	public static void main(String[] args) throws SQLException, CustomExpection, IllegalAccessException {
+
+		Dimension dm = new Dimension();
+		dm.setHeight(320);
+		dm.setWidth(1100);
+		Product p = new Product(13, "Mathankumar", "Santhanam", 300.0, LocalDateTime.now(), "This Art Designed by I",
+				"https://www.timeoutdubai.com/cloud/timeoutdubai/2021/09/11/udHvbKwV-IMG-Dubai-UAE-1-1200x800.jpg", dm,
+				"MANDANA");
+
+		System.out.println(deleteProduct(11));
+
+	}
+
+}
