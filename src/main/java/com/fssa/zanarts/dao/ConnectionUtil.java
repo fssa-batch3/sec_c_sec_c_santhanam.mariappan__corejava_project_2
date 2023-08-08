14a25081dbba5d8e5a123ce5f00e2b1ef50c2ed8
Santhanam <116251909+santhanam13@users.noreply.github.com>
2023-08-08 10:45:00 +0530

    Update ConnectionUtil.java

--- src/main/java/com/fssa/zanarts/dao/ConnectionUtil.java
+++ src/main/java/com/fssa/zanarts/dao/ConnectionUtil.java
@@ -12,21 +12,6 @@ import io.github.cdimascio.dotenv.Dotenv;
  * @throws RuntimeException If unable to connect to the database.
  */
 public class ConnectionUtil {
-//	public static Connection getConnection() {
-//		Connection con = null;
-//		String url = "jdbc:mysql://localhost:3306/Zanarts";
-//		String userName = "root";
-//		String passWord = "123456";
-//		try {
-//			Class.forName("com.mysql.cj.jdbc.Driver");
-//			con = DriverManager.getConnection(url, userName, passWord);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			throw new RuntimeException("Unable to connect to the database");
-//		}
-//		System.out.println("connection successful");
-//		return con;
-//	}
 
 	public static Connection getConnection() {
 		Connection con = null;
