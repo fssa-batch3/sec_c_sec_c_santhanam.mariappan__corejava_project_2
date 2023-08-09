17ae50e912ce810bc56e28f654ede203addb83b1
Santhanam <116251909+santhanam13@users.noreply.github.com>
2023-08-09 11:28:28 +0530

    Add files via upload

---
+++ src/main/java/com/fssa/zanarts/model/Dimension.java
@@ -0,0 +1,44 @@
+package com.fssa.zanarts.model;
+
+/**
+ * Represents the dimensions (height and width) of an object, such as a product
+ * or an image.
+ */
+
+public class Dimension {
+
+	private int height;
+	private int width;
+
+	public Dimension(int height, int width) {
+		this.height = height;
+		this.width = width;
+	}
+
+	/**
+	 * Constructs a new Dimension instance with specified height and width.
+	 *
+	 * @param height The height value.
+	 * @param width  The width value.
+	 */
+	public Dimension() {
+		  
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+}
---
+++ src/test/java/com/fssa/zanarts/dao/package-info.java
@@ -0,0 +1,1 @@
+package com.fssa.zanarts.dao;
