ce0563596989a7e5eb2e2f913690e4f34216547b
Santhanam <116251909+santhanam13@users.noreply.github.com>
2023-08-09 22:57:48 +0530

    Add finally clause in connection.util

--- src/main/java/com/fssa/zanarts/dao/ConnectionUtil.java
+++ src/main/java/com/fssa/zanarts/dao/ConnectionUtil.java
@@ -1,9 +1,6 @@
 package com.fssa.zanarts.dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import com.fssa.zanarts.customexception.ProductExpection;
 import com.fssa.zanarts.logger.Logger;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -13,46 +10,56 @@ import io.github.cdimascio.dotenv.Dotenv;
  */
 public class ConnectionUtil {
 
-	/**
-	 * Establishes a database connection.
-	 *
-	 * @return A Connection object representing the database connection.
-	 * @throws ProductExpection If unable to connect to the database.
-	 */
-	public static Connection getConnection() throws ProductExpection {
-		Connection con = null;
-		String url;
-		String userName;
-		String passWord;
-		if (System.getenv("CI") != null) {
-			url = System.getenv("DATABASE_HOST");
-			userName = System.getenv("DATABASE_USERNAME");
-			passWord = System.getenv("DATABASE_PASSWORD");
-		} else {
-			Dotenv env = Dotenv.load();
-			url = env.get("DATABASE_HOST");
-			userName = env.get("DATABASE_USERNAME");
-			passWord = env.get("DATABASE_PASSWORD");
-		}
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(url, userName, passWord);
-			Logger.info("Connected to database");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			throw new ProductExpection("JDBC Driver not found");
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new ProductExpection("Unable to connect to the database");
-		}
-		return con;
-	}
+    /**
+     * Establishes a database connection.
+     *
+     * @return A Connection object representing the database connection.
+     * @throws ProductExpection If unable to connect to the database.
+     */
+    public static Connection getConnection() throws ProductExpection {
+        Connection con = null;
+        String url;
+        String userName;
+        String passWord;
+        try {
+            if (System.getenv("CI") != null) {
+                url = System.getenv("DATABASE_HOST");
+                userName = System.getenv("DATABASE_USERNAME");
+                passWord = System.getenv("DATABASE_PASSWORD");
+            } else {
+                Dotenv env = Dotenv.load();
+                url = env.get("DATABASE_HOST");
+                userName = env.get("DATABASE_USERNAME");
+                passWord = env.get("DATABASE_PASSWORD");
+            }
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, userName, passWord);
+            Logger.info("Connected to database");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new ProductExpection("JDBC Driver not found");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ProductExpection("Unable to connect to the database");
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return con;
+    }
 
-	public static void main(String[] args) throws ProductExpection, SQLException {
-		try (Connection connection = getConnection()) {
-			// Use the database connection
-		} catch (ProductExpection e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) throws ProductExpection {
+        try {
+            Connection connection = getConnection();
+            // Use the database connection
+        } catch (ProductExpection e) {
+            e.printStackTrace();
+        }
+    }
 }
